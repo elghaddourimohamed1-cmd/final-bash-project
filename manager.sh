e52c3dc38cb8abbe1bf86560e9c61b637cc090e2
elghaddourimohamed1 <elghaddourimohamed1@gmail.com>
2026-04-17 18:31:21 +0200

    first bash creation project off course there is coming changes in future

---
+++ manager.sh
@@ -0,0 +1,45 @@
+#read -p "student manager " students
+#while true 
+
+#read -p "Add show search Exit " students
+#echo $students >> students.txt
+## grep	Searching for content inside files	grep "error" access.log
+##echo "Searching for a student 	grep "student name" students.txt "
+
+
+#read -p "enter a name " name
+read -p "what is your age " age
+#read -p "type one of the following options (add student-show-search-exit) " options
+if [ $age -lt 18 ]; then
+	echo "Student is Minor (not saved) "
+else
+	while true 
+	do
+	read -p "type one of the following options (add-show-search-exit) " options
+	if [ $options = "add" ]; then
+		read -p "enter your name " name
+		read -p "enter your age " age
+		echo $name " - " $age >> students.txt
+	elif [ $options = "show" ]; then
+		cat students.txt
+	
+#	while true
+#	do
+	elif [ $options = search ]; then
+		read -p "type your name to find your age in database " search
+		if grep -i "$search" students.txt; then echo ""
+		elif
+			echo "Not Found"
+		fi
+	else [ "$options" = "exit" ]
+	#	exit 1
+		break
+	fi
+	done
+			
+
+fi
+
+#To display the contents of a file in Bash use     cat students.txt
+
+
